7c7215cc38e8311311ffc6e9206ee6f8d8240a67
Toni Tertsonen <toni@tertsonen.xyz>
2025-07-31 23:09:20 +0300

    feat(tf): add gitlab backend config

--- main.tf
+++ main.tf
@@ -20,7 +20,13 @@ terraform {
     }
   }
   backend "http" {
-    
+    address="https://gitlab.com/api/v4/projects/72135661/terraform/state/cluster"
+    lock_address="https://gitlab.com/api/v4/projects/72135661/terraform/state/cluster/lock"
+    unlock_address="https://gitlab.com/api/v4/projects/72135661/terraform/state/cluster/lock"
+    username="token"
+    lock_method="POST"
+    unlock_method="DELETE"
+    retry_wait_min="5"
   }
 }
 
