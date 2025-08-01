fcc9e7a58b4d48908cdb9818af2d81f20db07f63
Toni Tertsonen <toni@tertsonen.xyz>
2025-08-02 00:17:44 +0300

    chore: remove old cluster stuff

--- k3s/bootstrap/templates/kubernetes/apps/system-upgrade/talos/.mjfilter.py
+++
@@ -1,7 +0,0 @@
-main = lambda data: (
-    data.get("distribution", {}).get("type", "k3s") in ["talos"] and
-        data.get("distribution", {})
-            .get("talos", {})
-            .get("schematics", {})
-            .get("enabled", False) == True
-)
