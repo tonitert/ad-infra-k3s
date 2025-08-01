fed0770291d5ce72c8691e14098941a292745aef
Toni Tertsonen <toni@tertsonen.xyz>
2025-08-02 02:25:00 +0300

    feat: use kube-hetzner (#5)
    * chore: remove old cluster stuff
    
    * Squashed 'kube-hetzner/' content from commit dcdc4a05
    
    git-subtree-dir: kube-hetzner
    git-subtree-split: dcdc4a05328d815f68c6fa25d1226ad7e2190049
    
    * feat: use kube-hetzner
    
    * fix: add default for ssh_key_path
    
    * ci: write keys to file
    
    * fix: add http backend
    
    * ci: create .ssh
    
    * ci: fix keys

--- docker/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "docker_host" {
-  description = "The Docker host address"
-  type        = string
-}
-
-variable "docker_key" {
-  description = "The Docker private key path"
-  type        = string
-}
--- ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -i keys/user_ad_server_ssh_key debian@$(cat ip_address.txt)
