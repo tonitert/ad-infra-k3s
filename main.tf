27e83d6c00c6afb9b2389a1492af10da2836ec54
Toni Tertsonen <toni@tertsonen.xyz>
2025-08-02 03:03:23 +0300

    ci: add gh token

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
 terraform {
   backend "http" {}
+
+  required_providers {
+    github = {
+        source = "integrations/github"
+        version = "6.6.0"
+    }
+  }
 }
 
 # Fix problem with GitHub Actions
-provider "github" {}
+provider "github" {
+    token = var.github_token
+}
+
+variable "github_token" {
+    sensitive = true
+    default = null
+}
